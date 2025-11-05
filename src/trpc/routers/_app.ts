6c97434b3c24087291edd87ec436117de278bcf2
miao-pqz <1125583676.qq.com>
2025-11-05 13:53:59 +0800

    06: background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,25 @@
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 
 import { createTRPCRouter, protectedProcedure } from '../init';
 
 export const appRouter = createTRPCRouter({
-  getAccounts: protectedProcedure
-    .query(({ ctx }) => {
-      return prisma.account.findMany({
-        where: {
-          userId: ctx.auth.user.id,
+  getWorkflows: protectedProcedure
+    .query(() => {
+      return prisma.workflow.findMany();
+    }),
+  createWorkflow: protectedProcedure
+    .mutation(async () => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          email: "johndoe@example.com",
         },
       });
+
+      return { success: true, message: "Job queued" };
     }),
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
