cc2ae67392a1870fb1c8bf0ffdd055e02a29539d
miao-pqz <1125583676.qq.com>
2025-11-11 14:04:29 +0800

    16: node selector

---
+++ src/features/triggers/components/base-trigger-node.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { memo } from "react";
+import Image from "next/image";
+import type { LucideIcon } from "lucide-react";
+import { type NodeProps, Position } from "@xyflow/react";
+
+import { WorkflowNode } from "@/components/workflow-node";
+import { BaseHandle } from "@/components/react-flow/base-handle";
+import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
+
+interface Props extends NodeProps {
+    icon: LucideIcon | string;
+    name: string;
+    description?: string;
+    children?: React.ReactNode;
+    // status: NodeStatus,
+    onSettings?: () => void;
+    onDoubleClick?: () => void;
+};
+
+export const BaseTriggerNode = memo(({
+    id,
+    icon: Icon,
+    name,
+    description,
+    children,
+    onSettings,
+    onDoubleClick,
+}: Props) => {
+    // TODO: Add delete method
+    const handleDelete = () => {};
+
+    return (
+        <WorkflowNode
+            name={name}
+            description={description}
+            onDelete={handleDelete}
+            onSettings={onSettings}
+        >
+            {/* TODO: Wrap within NodeStatusIndicator */}
+            <BaseNode
+                onDoubleClick={onDoubleClick}
+                className="rounded-l-2xl relative group"
+            >
+                <BaseNodeContent>
+                    {typeof Icon === "string" ? (
+                        <Image src={Icon} alt={name} width={16} height={16} />
+                    ) : (
+                        <Icon className="size-4 text-muted-foreground" />
+                    )}
+                    {children}
+                    <BaseHandle
+                        id="source-1"
+                        type="source"
+                        position={Position.Right}
+                    />
+                </BaseNodeContent>
+            </BaseNode>
+        </WorkflowNode>
+    );
+});
+
+BaseTriggerNode.displayName = "BaseTriggerNode";
