226dd5b000f0e0688da089cff8d92428469fd64b
miao-pqz <1125583676.qq.com>
2025-11-11 10:21:52 +0800

    15: editor setup

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,13 +1,78 @@
 "use client";
 
+import { useState, useCallback } from "react";
+import {
+    type Node,
+    type Edge,
+    type NodeChange,
+    type EdgeChange,
+    type Connection,
+    ReactFlow,
+    applyNodeChanges,
+    applyEdgeChanges,
+    addEdge,
+    Background,
+    Controls,
+    MiniMap,
+    Panel,
+} from "@xyflow/react";
+
+import { nodeComponents } from "@/config/node-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+
+import { AddNodeButton } from "./add-node-button";
+
+import "@xyflow/react/dist/style.css";
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
     const { data: workflow } = useSuspenseWorkflow(workflowId);
 
+    const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+    const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => 
+            setNodes(
+                (nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)
+            ),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => 
+            setEdges(
+                (edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)
+            ), 
+        [],
+    );
+    const onConnect = useCallback(
+        (params: Connection) => 
+            setEdges(
+                (edgesSnapshot) => addEdge(params, edgesSnapshot)
+            ),
+        [],
+    );
+
     return (
-        <p>
-            {JSON.stringify(workflow, null, 2)}
-        </p>
+        <div className="size-full">
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                nodeTypes={nodeComponents}
+                proOptions={{
+                    hideAttribution: true,
+                }}
+                fitView
+            >
+                <Background />
+                <Controls />
+                <MiniMap />
+                <Panel position="top-right">
+                    <AddNodeButton />
+                </Panel>
+            </ReactFlow>
+        </div>
     );
 };
