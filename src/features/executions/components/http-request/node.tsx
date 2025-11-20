38f5f95f7719a48814c462beea8e8fdfd2eb08c3
miao-pqz <1125583676.qq.com>
2025-11-20 12:02:51 +0800

    19: node variables

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -9,6 +9,7 @@ import { BaseExecutionNode } from "../base-execution-node";
 import { HttpRequestDialog, type HttpRequestFormValues } from "./dialog";
 
 type HttpRequestNodeData = {
+    variableName?: string;
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
